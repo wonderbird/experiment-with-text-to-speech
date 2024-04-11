227251d7578d28546bb184938a3dac3057d03da3
Stefan Boos <kontakt@boos.systems>
2024-04-11 06:04:19 +0200

    feat: convert text to audio
    following the guide at https://platform.openai.com/docs/guides/text-to-speech

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,12 +2,10 @@ import "chai/register-should";
 import {config} from "chai";
 import {describe, it} from "@jest/globals";
 
-import {Greeter} from "../src";
-
 config.truncateThreshold = 0;
 
-describe("Greeter", () => {
-  it("returns Hello World!", () => {
-    new Greeter().greet().should.equal("Hello World!");
+describe("Nothing to see here", () => {
+  it("please leave", () => {
+    expect(true).toBe(true);
   });
 });
